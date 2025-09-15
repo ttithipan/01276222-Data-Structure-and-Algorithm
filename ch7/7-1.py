75ce2bbe20eb42edf1698ac3eec09c23695526ee
Aspirin <90672568+ttithipan@users.noreply.github.com>
2025-09-15 14:00:56 +0700

    Create 7-1.py

---
+++ ch7/7-1.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
+            return self.root
+        else:
+            return self._insert(self.root, data)
+    
+    def _insert(self, node, data):
+        if data < node.data:
+            if node.left is None:
+                node.left = Node(data)
+            else:
+                self._insert(node.left, data)
+        else:
+            if node.right is None:
+                node.right = Node(data)
+            else:
+                self._insert(node.right, data)
+        return self.root
+
+    def printTree(self, node, level = 0):
+        if node is not None:
+            self.printTree(node.right, level + 1)
+            print(' '+'     ' * level + str(node))
+            self.printTree(node.left, level + 1)
+
+# Main part
+T = BST()
+inp = [int(i) for i in input('Enter Input : ').split()]
+for i in inp:
+    root = T.insert(i)
+T.printTree(root)
